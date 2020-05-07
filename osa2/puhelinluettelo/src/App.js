15af8a9f28934a8440f4dac7a26e38511b17f3e4
Suvisp <suvi.s.partanen@gmail.com>
2020-05-07 17:37:25 +0300

    puhelinluettelo 2.18 PUT tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,39 +1,87 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import PeopleList from './components/PeopleList'
+import peopleService from './services/restclient'
 
 const App = () => {
   const [people, setPeople] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newPeople, setNewPeople] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
   const [searchResults, setSearchResults] = useState([])
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPeople(response.data)
+    peopleService
+      .getAll()
+      .then(initialPeople => {
+        setPeople(initialPeople)
       })
   }, [])
-  console.log('render', people.length, 'persons')
+  console.log('render', people.length, people)
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
   const handleChange = (event) => setSearchTerm(event.target.value)
 
-//lisää henkilö
-  const addPerson = () => {
-    //ilmoittaa, jos nimi on jo listalla ja estää lisäyksen
-    if (people.find(({ name }) => name === newName)) { alert(`${newName} is already added to the phonebook, replace it with a new one?`) }
-    else
-      setPeople([...people, { name: newName, number: newNumber }]);
-    setNewName('');
-    setNewName('');
+  //lisää henkilö
+  // const addPerson = () => {
+  //   //ilmoittaa, jos nimi on jo listalla ja estää lisäyksen
+  //   if (people.find(({ name }) => name === newName)) { alert(`${newName} is already added to the phonebook, replace it with a new one?`) }
+  //   else
+  //     setPeople([...people, { name: newName, number: newNumber }]);
+  //   setNewName('');
+  //   setNewName('');
+  // }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    //ilmoittaa, jos nimi on jo listalla ja kysyy halutaankp puhelinnumero muuttaa?
+    if (people.find(({ name }) => name === newName)) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        window.open("http://localhost:3000", "Phonenumber changed");
+        const id_updateNumber = people.find(({ name }) => name === newName).id
+        const personObject = {
+          name: newName,
+          number: newNumber,
+        }
+        peopleService
+          .updateOne(id_updateNumber, personObject)
+          .then(returnedPerson => {
+            setNewPeople(people.map(p => p.id_updateNumber !== id_updateNumber ? p : returnedPerson))
+          })
+          .catch(error => {
+            alert(
+              `the note '${people.name}' was already deleted from server`
+            )
+            setPeople(people.filter(p => p.id_updateNumber !== id_updateNumber))
+          })
+      }
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+        id: people.length + 1,
+      }
+      peopleService
+        .createOne(personObject)
+        .then(updatedPeople => {
+          setPeople(people.concat(updatedPeople))
+          setNewPeople('')
+        })
+    }
+  }
+
+  const removePerson = (id) => {
+    peopleService
+      .deleteOne(id)
+      .then(returnedPeople => {
+        console.log(returnedPeople)
+        setNewPeople(people.concat(returnedPeople))
+        // setPeople()
+      })
   }
 
   //filteröi nimet, sisältää annetun kirjaimen tai nimen
@@ -49,11 +97,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter 
-      searchTerm={searchTerm} 
-      onChange={handleChange}
-      searchResults={searchResults}
-        />
+      <Filter
+        searchTerm={searchTerm}
+        onChange={handleChange}
+        searchResults={searchResults}
+      />
       <h2>add a new</h2>
       <PersonForm onSubmit={addPerson}
         name={newName}
@@ -63,7 +111,7 @@ const App = () => {
       />
       {/* <div>debug: {newName} {newNumber}</div> */}
       <h2>Numbers</h2>
-      <PeopleList people={people}/>
+      <PeopleList people={people} removePerson={removePerson} />
     </div>
   )
 }
