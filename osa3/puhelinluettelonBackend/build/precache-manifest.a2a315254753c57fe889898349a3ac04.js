0b3165407aad8de4ed7b7f99b988eacc80737330
Suvisp <suvi.s.partanen@gmail.com>
2020-05-13 10:47:30 +0300

    3.9 puhelinluettelo toimii herokussa

---
+++ osa3/puhelinluettelonBackend/build/precache-manifest.a2a315254753c57fe889898349a3ac04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb5f1c185b18c7e6f24235221209cbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09f961541dadd0d5cb3c",
+    "url": "/static/css/main.950b48be.chunk.css"
+  },
+  {
+    "revision": "93b7a437827d2b6212f7",
+    "url": "/static/js/2.9c1c8f71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c1c8f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f961541dadd0d5cb3c",
+    "url": "/static/js/main.ee62285a.chunk.js"
+  },
+  {
+    "revision": "d00a55e58aad67ff89c6",
+    "url": "/static/js/runtime-main.01dd18a1.js"
+  }
+]);
--- osa3/puhelinluettelonBackend/index.js
+++ osa3/puhelinluettelonBackend/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
+
+app.use(express.static('build'))
 
 // app.use(morgan('tiny'))
 app.use(bodyParser.json())
@@ -131,7 +136,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
