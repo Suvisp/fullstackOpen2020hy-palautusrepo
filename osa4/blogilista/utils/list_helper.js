4e3845ad62a8fb17e6600354964073625957cdc0
Suvisp <suvi.s.partanen@gmail.com>
2020-05-28 22:23:35 +0300

    4.4 valmis

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,22 @@
 const dummy = (blogs) => {
-return 1
+  return 1
 }
-  
-  module.exports = {
-    dummy
+
+const totalLikes = (blog) => {
+  var total = 0
+  if (blog.length === 0) {
+    return 0
+  } if (blog.length === 1) {
+    return blog[0].likes
+  } else {
+    for (var i = 0; i < blog.length; i++) {
+      total += blog[i].likes
+    }
+    return total
   }
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
