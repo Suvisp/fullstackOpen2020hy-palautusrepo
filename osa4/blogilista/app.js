710df9f3c213c5a281787c1e885fef615a89c422
Suvisp <suvi.s.partanen@gmail.com>
2020-05-28 12:11:54 +0300

    4.2 blogilista valmis

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,12 +1,21 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 
-const mongoose = require('mongoose')
+logger.info('connecting to', config.MONGODB_URI)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connection to MongoDB:', error.message)
+	})
 
 app.use(cors())
 app.use(express.json())
