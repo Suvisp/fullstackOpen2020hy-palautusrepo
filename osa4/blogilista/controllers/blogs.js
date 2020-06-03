c876ef13936b5a6fdc5819d9b3a29b882adaef86
Suvisp <suvi.s.partanen@gmail.com>
2020-06-03 13:05:16 +0300

    4.13-14 delete ja put testaus valmis

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -3,7 +3,7 @@ const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
-    response.json(blogs.map(blog => blog.toJSON()))
+    response.json(blogs)
   })
   
  blogsRouter.post('/', async (request, response) => {
@@ -18,6 +18,25 @@ blogsRouter.get('/', async (request, response) => {
 
 	const savedBlog = await blog.save()
 		response.json(savedBlog.toJSON())
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+	const body = request.body
+
+	const blog = {
+    title: body.title,
+		author: body.author,
+    url: body.url,
+    likes: body.likes
+	}
+
+	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+	response.json(updatedBlog.toJSON())
 })
 
 
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -6,6 +6,8 @@ const helper = require('./test_helper')
 
 const Blog = require('../models/blog')
 
+describe('when there is initially some blogs saved', () => {
+
 beforeEach(async () => {
     await Blog.deleteMany({})
     await Blog.insertMany(helper.initialBlogs)
@@ -24,10 +26,15 @@ test('there are 2 blogs', async () => {
     expect(response.body).toHaveLength(2)
   })
 
-test('id is formatted as id', async () => {
+describe('viewing a specific blog', () => {
+
+test('succeeds with a valid id', async () => {
     const response = await api.get('/api/blogs')
     expect('id').toBeDefined()
 })
+})
+
+describe('addition of a new blog', () => {
 
 test('a valid blog can be added ', async () => {
     const newBlog = {
@@ -51,7 +58,7 @@ test('a valid blog can be added ', async () => {
       'Koodi tutoriaalit'
     )
   })
-
+  
   test('if no likes defined, likes is zero', async () => {
     const newBlog = {
       title: 'Rasvatonsokeritonmautondieetti',
@@ -88,7 +95,52 @@ test('a valid blog can be added ', async () => {
     const blogsAtEnd = await helper.blogsInDb()
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
   })
+})
 
+  describe('deletion of a blog', () => {
+	
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(
+        helper.initialBlogs.length - 1
+      )
+
+      const titles = blogsAtEnd.map(r => r.title)
+
+      expect(titles).not.toContain(blogToDelete.title)
+    })
+  })
+
+  describe('updating a specific blog', () => {
+	
+    test('likes is updated', async () => {
+    const blogs = await helper.blogsInDb()
+    const blogToUpdate = blogs[1]
+ 
+    blogToUpdate.likes = 13
+   
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(blogToUpdate)
+ 
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(blogs.length)
+ 
+    const blogsWithUpdate = await helper.blogsInDb()   
+    expect(blogsWithUpdate[1].likes).toBe(13)
+
+    })
+  })
+
+})
 
 afterAll(() => {
   mongoose.connection.close()
