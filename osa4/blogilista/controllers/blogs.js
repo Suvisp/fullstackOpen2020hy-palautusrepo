d3b8e22df2d0eb725ae9b69933ce678084812cdb
Suvisp <suvi.s.partanen@gmail.com>
2020-06-06 17:16:34 +0300

    4.20 asti tehty

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -10,38 +10,39 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
   })
 
-  const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-      return authorization.substring(7)
-    }
-    return null
-  }
-  
- blogsRouter.post('/', async (request, response) => {
+ blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
-    const token = getTokenFrom(request)
 
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    
+    if (!request.token || !decodedToken.id) {
+        return response.status(401).send({ error: 'Missing or invalid token' }) 
+    }
+    const user = await User.findById(decodedToken.id)
 
-	const blog = new Blog({
-		title: body.title,
-		author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-    user: user._id
-  })
+    const blog = new Blog({ 
+        title: body.title,
+        author: body.author,
+        user: user,
+        url: body.url,
+        likes: body.likes
+    })
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+    const savedBlog = await blog.save()
+    if (savedBlog) {
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+        response.status(201).json(savedBlog)
+    } else {
+        response.status(400).end()
+    }
 
-		response.json(savedBlog.toJSON())
+} catch (exception) {
+    next(exception)
+}
 })
+
 
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
