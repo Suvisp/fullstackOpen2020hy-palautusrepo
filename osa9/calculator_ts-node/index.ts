a41d896cde7ba194fc2d0ae12061216dedbe12b5
Suvisp <suvi.s.partanen@gmail.com>
2020-07-13 17:20:01 +0300

    9.6-7 EsLint ja WebExercises valmis

--- osa9/calculator_ts-node/index.ts
+++ osa9/calculator_ts-node/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 const app = express();
 
 import { bmiCalculator } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
 app.use(express.json())
 
@@ -13,16 +14,30 @@ app.get('/hello', (_req, res) => {
 
 app.get('/bmi', (req, res) => {
   try {
-  let object = new Object({
-    weight: req.query.weight,
-    height: req.query.height,
-    bmi: bmiCalculator(Number(req.query.height), Number(req.query.weight))
-  })
-  res.send(object);
-} catch (err) {
-  res.send({ error: "malformatted parameters" })
-}
+    let object = new Object({
+      weight: req.query.weight,
+      height: req.query.height,
+      bmi: bmiCalculator(Number(req.query.height), Number(req.query.weight))
+    })
+    res.send(object);
+  } catch (err) {
+    res.send({ error: "malformatted parameters" })
+  }
 });
+
+app.post('/exercises', (req, res) => {
+  const { daily_exercises, target } = req.body
+  if ( !daily_exercises || !target) {
+    res.send({ error: "parameters missing" })
+  }
+  if (!(daily_exercises instanceof Array) || !(typeof target === 'number')) {
+  // else if ( isNaN(daily_exercises) || isNaN(target)) {
+    res.send({ error: "malformatted parameters" })
+  }
+
+  let resultValues = calculateExercises(target, daily_exercises)
+  res.json(resultValues)
+})
 
 const PORT = 3002;
 
